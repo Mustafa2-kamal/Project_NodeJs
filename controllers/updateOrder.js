f6a3a70d6993ab7fc31fd8f9099cc64797b5acd4
Mustafa2-kamal <mostafakamalsokar@gmail.com>
2023-06-01 15:43:20 +0300

    first commit www

--- controllers/updateOrder.js
+++ controllers/updateOrder.js
@@ -1,13 +1,69 @@
 const Order = require('../models/order.js');
+const user = require('../models/user');
+
 
 
 const handleUpdateOrder = async (req, res) => {
 
-    console.log(req.body);
+
+    const status=req.body.status2;
+    const email=req.body.email;
+    const estimatedTime=req.body.estimatedTime;
+
 
  
     try {
-        console.log(req.params.orderId);
+     
+        if(status=='working') {
+            const found = await user.find({ role: 'worker',email:email}).exec();
+            const date=found.availableTime;
+            const datenew=new Date();
+
+
+            if(datenew>date){
+
+                let startTime=datenew;
+
+                const endingDate = new Date(startTime.getTime() + estimatedTime * 60 * 60 * 1000);
+
+
+                const result = await Order.findOneAndUpdate({_id:req.params.orderId}, {
+                    startingTime:startTime,endingTime:endingDate
+                },{new:true});
+
+
+                const result2 = await user.findOneAndUpdate({role: 'worker',email:email}, {
+                    availableTime:endingDate
+                },{new:true});
+
+                result.save();
+                result2.save();
+
+
+
+            }
+            else{
+
+                let startTime=date;
+
+                const endingDate = new Date(startTime.getTime() + estimatedTime * 60 * 60 * 1000);
+
+                const result = await Order.findOneAndUpdate({_id:req.params.orderId}, {
+                    startingTime:startTime,endingTime:endingDate
+                },{new:true});
+
+
+                const result2 = await user.findOneAndUpdate({role: 'worker',email:email}, {
+                    availableTime:endingDate
+                },{new:true});
+
+                result.save();
+                result2.save();
+
+            }
+
+        }
+
 
         const result = await Order.findOneAndUpdate({_id:req.params.orderId}, req.body,{new:true});
    
