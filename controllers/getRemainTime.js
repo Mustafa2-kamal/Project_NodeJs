d6a9f82561a909d3dd4daa469d180d56cb7987ff
Mustafa2-kamal <mostafakamalsokar@gmail.com>
2023-06-01 20:57:50 +0300

    first commi wwy

--- controllers/getRemainTime.js
+++ controllers/getRemainTime.js
@@ -5,19 +5,19 @@ const Order = require('../models/order.js');
 const handleGetRemainTime = async (req, res) => {
 
 
-    const status=req.body.status2;
 
 
 
     try {
      
- 
-        if(status=='working'){
 
-            let newDate=new Date();
-    
+            let newDate=new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Gaza' }));
 
                 const found = await Order.findOne({_id:req.params.orderId});
+
+                const status=found.status2;
+
+                if(status=='working'){
 
 
                 const remainingTime = found.endingTime.getTime() - newDate.getTime();
@@ -39,6 +39,8 @@ const handleGetRemainTime = async (req, res) => {
                 result.save();
                 res.status(200).json({time:remainingHours+'h:'+remainingMinutes+'m'});
             }
+
+            res.status(200).json({});
 
     } catch (err) {
         res.status(500).json({ 'message': err.message });
